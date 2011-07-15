7cf56b7e11895b9abdd8dc627e9f3f193c26b01a
Jim Ursetto <jim@3e8.org>
2011-07-15 18:37:21 -0500

    Add router to rep example (rtpapa)

---
+++ rtpapa.scm
@@ -0,0 +1,34 @@
+;;
+;;  Custom routing Router to Papa (ROUTER to REP)
+;;
+(use zmq)
+(include "zhelpers.scm")
+
+;;  We will do this all in one thread to emphasize the sequence
+;;  of events...
+
+(define client (make-socket 'xrep))        ;; FIXME: ROUTER
+(bind-socket client "ipc://routing.ipc")
+
+(define worker (make-socket 'rep))
+(socket-option-set! worker 'identity "A")
+(connect-socket worker "ipc://routing.ipc")
+
+;; Wait for the worker to connect so that when we send a message
+;; with routing envelope, it will actually match the worker...
+(thread-sleep! 1)
+
+;; Send papa address, address stack, empty part, and request
+(send-multipart-message
+ client
+ "A" "address 3" "address 2" "address 1" ""
+ "This is the workload")
+
+;; Worker should just get the workload
+(dump-socket worker)
+
+;; We don't play with envelopes in the worker
+(send-message worker "This is the reply")
+
+;; Now dump what we got off the ROUTER socket...
+(dump-socket client)
